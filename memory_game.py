ab64f2515d184be954fec6b4934a5a0b8e73f099
marcioszka <monkiewicz.marta@gmail.com>
2022-12-03 16:29:02 +0100

    Field position defined

--- memory_game.py
+++ memory_game.py
@@ -2,6 +2,23 @@ import os
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+def get_user_position(width, height):
+    while True:
+        position = input("Enter field position: ")
+        try:
+            if not position[0].isalpha() or not position[1].isnumeric():
+                raise ValueError
+            if int(position[1]) < 1 or int(position[1]) > height:
+                raise ValueError
+            letter = position[0].lower()
+            column = ord(letter) - 97
+            row = int(position[1])-1
+            if not column < width:
+                raise ValueError
+        except ValueError:
+            print("Position is wrong, try again!")
+        return (row, column)
+
 # clears the screen
 def console_clear():
     os.system('cls' if os.name == 'nt' else 'clear')
