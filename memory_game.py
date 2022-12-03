7b944d0e7c389a5a560d2729b90dd4c6fa67d9cb
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2022-12-03 15:21:44 +0100

    Implement get_hidden_board

--- memory_game.py
+++ memory_game.py
@@ -2,6 +2,7 @@ import os
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 difficulties = {'easy': (5,4), 'medium': (5,6), "hard": (5,10)}
+
 # clears the screen
 def console_clear():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -23,6 +24,10 @@ def get_difficulty_level():
         else:
             print('\nThere is no sush difficulty level!',end='\n\n')
     return choice.lower()
+
+
+def get_hidden_board(width, height):
+    return [[False for _ in range(height)] for _ in range(width)]
 
 
 def main():
